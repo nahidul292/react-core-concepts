44470268c3c34f44ea6c45937049a97293268eec
Nahidul Islam <nahidul292@gmail.com>
2021-02-24 19:02:31 +0600

    second

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: 'Shamim',
+    age: 34,
+    salary: 75000,
+    ye: 'Male'
+  }
+  var person2 = {
+    name: 'Eva',
+    age: 24,
+    salary: 75000,
+    ye: 'Female'
+  }
+  var style = {
+    color:'red',
+    backgroundColor: 'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +25,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1 className="" style={style}> My heading is {person.name + " " + person.age}</h1>
+        <h2>Singer: {person2.name  + " " + person2.age}</h2>
       </header>
     </div>
   );
